f48692df56e4cb9ce43d631527f031feecaa47ea
inamali-palasaniya <inamali@intiqa.io>
2025-11-28 15:46:56 +0530

    Fix Admin build: Replace Grid with Box/CSS Grid

--- admin/src/app/page.tsx
+++ admin/src/app/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 
@@ -10,38 +9,32 @@ export default function Home() {
       <Typography variant="h4" gutterBottom>
         Dashboard
       </Typography>
-      <Grid container spacing={3}>
-        <Grid item xs={12} md={4}>
-          <Paper sx={{ p: 3, display: 'flex', flexDirection: 'column', height: 140 }}>
-            <Typography component="h2" variant="h6" color="primary" gutterBottom>
-              Total Kitabs
-            </Typography>
-            <Typography component="p" variant="h4">
-              1
-            </Typography>
-          </Paper>
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <Paper sx={{ p: 3, display: 'flex', flexDirection: 'column', height: 140 }}>
-            <Typography component="h2" variant="h6" color="primary" gutterBottom>
-              Total Surahs
-            </Typography>
-            <Typography component="p" variant="h4">
-              114
-            </Typography>
-          </Paper>
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <Paper sx={{ p: 3, display: 'flex', flexDirection: 'column', height: 140 }}>
-            <Typography component="h2" variant="h6" color="primary" gutterBottom>
-              Total Reciters
-            </Typography>
-            <Typography component="p" variant="h4">
-              1
-            </Typography>
-          </Paper>
-        </Grid>
-      </Grid>
+      <Box sx={{ display: 'grid', gridTemplateColumns: { xs: '1fr', md: 'repeat(3, 1fr)' }, gap: 3 }}>
+        <Paper sx={{ p: 3, display: 'flex', flexDirection: 'column', height: 140 }}>
+          <Typography component="h2" variant="h6" color="primary" gutterBottom>
+            Total Kitabs
+          </Typography>
+          <Typography component="p" variant="h4">
+            1
+          </Typography>
+        </Paper>
+        <Paper sx={{ p: 3, display: 'flex', flexDirection: 'column', height: 140 }}>
+          <Typography component="h2" variant="h6" color="primary" gutterBottom>
+            Total Surahs
+          </Typography>
+          <Typography component="p" variant="h4">
+            114
+          </Typography>
+        </Paper>
+        <Paper sx={{ p: 3, display: 'flex', flexDirection: 'column', height: 140 }}>
+          <Typography component="h2" variant="h6" color="primary" gutterBottom>
+            Total Reciters
+          </Typography>
+          <Typography component="p" variant="h4">
+            1
+          </Typography>
+        </Paper>
+      </Box>
     </Box>
   );
 }
